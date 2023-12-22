567d3fed10d11572b9bbf6502f5ae5ff64a98116
AndrejPitomec <148689748+AndrejPitomec@users.noreply.github.com>
2023-12-22 09:54:57 +0100

    Add files via upload

---
+++ Stepeni/src/Stepeni/Stepeni.java
@@ -0,0 +1,20 @@
+package Stepeni;
+
+import java.util.Scanner;
+public class Stepeni {
+
+	public static void main(String[] args) {
+		Scanner tastatura = new Scanner(System.in);
+		//Vcituvanje na stepeni vo Celzijus od korisnikot
+		System.out.print("Vnesete Celziusovi stepeni:");
+		c = tastatura.nextInt();
+		//Presmetka na stepeni vo Farenhajt po formula
+	    f= 9*c/5+32;
+	    //Prikazuvanje na rezultat na ekran
+	    System
+	 
+		
+
+	}
+
+}
